dd258e3ae5fc4234212039d3c794eef03d0cf7fa
zina-frid <54913858+zina-frid@users.noreply.github.com>
2021-04-22 17:15:39 +0300

    Add files via upload

---
+++ lab4/lib/merge.h
@@ -0,0 +1,8 @@
+#ifndef MERGE_H
+#define MERGE_H
+
+#include <stddef.h>
+
+extern unsigned* merge(const unsigned arrayA[], size_t lengthA, const unsigned arrayB[], size_t lengthB);
+
+#endif //MERGE_H
