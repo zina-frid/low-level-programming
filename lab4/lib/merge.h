74616136a1eaee819d40224315504d3429e4a085
zina-frid <54913858+zina-frid@users.noreply.github.com>
2021-04-22 17:15:07 +0300

    Delete lab4/lib directory

--- lab4/lib/merge.h
+++
@@ -1,8 +0,0 @@
-#ifndef MERGE_H
-#define MERGE_H
-
-#include <stddef.h>
-
-extern unsigned* merge(const unsigned arrayA[], size_t lengthA, const unsigned arrayB[], size_t lengthB);
-
-#endif //MERGE_H
